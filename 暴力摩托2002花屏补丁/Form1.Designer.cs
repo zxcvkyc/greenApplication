02682f4436874e04ddf16e21842b83418d483bea
zxcvkyc <53789246+zxcvkyc@users.noreply.github.com>
2023-01-26 17:55:36 +0800

    test
    test

---
+++ 暴力摩托2002花屏补丁/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace 暴力摩托2002花屏补丁
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_startGame = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_startGame
+            // 
+            this.btn_startGame.Location = new System.Drawing.Point(57, 76);
+            this.btn_startGame.Name = "btn_startGame";
+            this.btn_startGame.Size = new System.Drawing.Size(147, 50);
+            this.btn_startGame.TabIndex = 0;
+            this.btn_startGame.Text = "开始游戏";
+            this.btn_startGame.UseVisualStyleBackColor = true;
+            this.btn_startGame.Click += new System.EventHandler(this.btn_startGame_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Location = new System.Drawing.Point(57, 147);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(147, 50);
+            this.btn_Close.TabIndex = 1;
+            this.btn_Close.Text = "恢复桌面图标";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 280);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.btn_startGame);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "暴力摩托2002花屏补丁";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_startGame;
+        private System.Windows.Forms.Button btn_Close;
+    }
+}
+
---
+++ 暴力摩托2002花屏补丁/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 暴力摩托2002花屏补丁
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_startGame_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                RunPro("Roadrash");
+                if (IsContainsProcess("explorer"))
+                {
+                    KillProcess("explorer");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("请把本补丁放在游戏目录中运行");
+            }
+
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            CloseGame();
+        }
+        /// <summary>
+        /// 运行exe
+        /// </summary>
+        /// <param name="exeName">绝对路径或相对路径</param>
+        public void RunPro(string exeName)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = exeName;
+            process.Start();
+        }
+        /// <summary>
+        /// 判断是否存在指定的进程
+        /// </summary>
+        /// <param name="processName">要判断的进程名称</param>
+        /// <returns>是否存在的结果</returns>
+        public bool IsContainsProcess(string processName)
+        {
+            Process[] prolist = Process.GetProcessesByName(processName);
+            foreach (var item in prolist)
+            {
+                if (item.ProcessName.Equals(processName))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// 调用任务管理器结束进程
+        /// </summary>
+        /// <param name="pricessName">要结束的进程名</param>
+        public void KillProcess(string pricessName)
+        {
+            pricessName += ".exe";
+            // explorer.exe
+            //Process.Start("taskkill.exe", " /f /im explorer.exe");
+            Process.Start("taskkill.exe", " /f /im " + pricessName);
+        }
+        public void CloseGame()
+        {
+            if (IsContainsProcess("Roadrash"))
+            {
+                KillProcess("Roadrash");
+            }
+            if (IsContainsProcess("explorer"))
+            {
+                // 如果已经打开了，什么都不用做
+            }
+            else
+            {
+                RunPro("C:\\windows\\explorer");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseGame();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Process.Start("taskkill.exe", " /f /im explorer.exe");
+        }
+    }
+}
